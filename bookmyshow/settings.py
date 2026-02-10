a7ddb4e3a76a19b142662ff8eabb18d1bab227ae
Sridharan-AR1 <sridharan@joshtechnologygroup.com>
2026-02-10 18:50:19 +0530

    SA_A3_07 : CORS setup

--- bookmyshow/settings.py
+++ bookmyshow/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt.token_blacklist",
     "django_filters",
     "drf_spectacular",
+    "corsheaders",
     "apps.base",
     "apps.users",
     "apps.movies",
@@ -57,6 +58,7 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = "users.User"
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -167,3 +169,13 @@ SPECTACULAR_SETTINGS = {
         "BearerAuth": {"type": "http", "scheme": "bearer", "bearerFormat": "JWT"}
     },
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+]
