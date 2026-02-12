jtg-inductions-fe/BE-Sridharan-A-UID00666-A3

SA_A3_05 : Slot API - added endpoint for getting booked seats of particular Slot

bookmyshow/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path("auth/", include("apps.users.urls")),
     path("movies/", include("apps.movies.urls")),
     path("cinemas/", include("apps.cinemas.urls")),
+    path("slots/", include("apps.slots.urls")),
 ]
 
 if settings.DEBUG:

jtg-inductions-fe/BE-BookMyShow-Sridharan-A

bookmyshow/urls.py
@@ -28,7 +28,8 @@ urlpatterns = [
             [
                 path("", include("apps.users.urls")),
                 path("movies/", include("apps.movies.urls")),
-                path("cinemas/", include("apps.cinemas.urls"))
+                path("cinemas/", include("apps.cinemas.urls")),
+                path("slots/", include("apps.slots.urls"))
             ]
         ),
     ),